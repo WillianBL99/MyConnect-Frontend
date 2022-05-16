103a105dc9e3db42dcd708373f40443a64752c73
paulo uilian <uilianlbarros@gmail.com>
2022-05-16 14:44:27 -0300

    Implementará função de logout

--- src/utils/setCurrentWindow.js
+++ src/utils/setCurrentWindow.js
@@ -4,8 +4,13 @@ function setWindow(setStates, windowsState, setWindowsState, name){
   let window = windowsState.window;
 
   if(name === 'info_product') setStates({menuOpen: false});
-  if(name === 'store') windowOpen = false;
-  else if( name !== window) windowOpen = true; //TODO: Implementar setTimeout para fechar e abrir outra janela
+
+  if(name === 'store'){
+    windowOpen = false;
+
+  } else if( name !== window) {
+    windowOpen = true; //TODO: Implementar setTimeout para fechar e abrir outra janela
+  }
 
   if (rotes.includes(name)) {
     window = name;
